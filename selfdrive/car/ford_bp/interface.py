adfab6b453dcd488859292e495a573dfd143835e
Daniel A. Bergamini <daniel.bergamini@gmail.com>
2026-01-12 20:34:25 -0500

    Wire ford_bp interface to legacy params and BP controller/state

--- selfdrive/car/ford_bp/interface.py
+++ selfdrive/car/ford_bp/interface.py
@@ -1,2 +1,11 @@
-# Placeholder Bluepilot Ford stack. Proxy to Flow Pilot implementation for now.
-from selfdrive.car.ford.interface import CarInterface  # noqa: F401
+from selfdrive.car.interfaces import CarInterfaceBase
+from selfdrive.car.ford.interface import CarInterface as LegacyCarInterface
+from selfdrive.car.ford_bp.carcontroller import CarController
+from selfdrive.car.ford_bp.carstate import CarState
+
+
+class CarInterface(CarInterfaceBase):
+  _get_params = LegacyCarInterface._get_params
+
+  def __init__(self, CP, CarControllerUnused, CarStateUnused):
+    super().__init__(CP, CarController, CarState)
